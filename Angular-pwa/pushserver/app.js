8eeb6fe388ec78887ef14869d3f14b2352693997
BABA 1995 <fakruddinbaba16@gmail.com>
2020-10-02 03:09:35 +0530

    Swupdate completed pending with push notification

---
+++ Angular-pwa/pushserver/app.js
@@ -0,0 +1,5 @@
+const webpush = require('web-push');
+
+
+const publicVapidKey = 'BDw9rsFllBr0w5Yu7f6lgC8O_yAF4QLOmrHDin8MR3cikeJOQcZNpzSlEM4RUUajWz2n8L52wCyLSFWemGuyTJI ';
+const privateVapidKey = 'lwrZqXSlsO-eC_kMi8IjRo53pN1p-165hZUA9mzlReY';
--- Angular-pwa/src/app/app.component.ts
+++ Angular-pwa/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import  { SwPush, SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +10,13 @@ export class AppComponent {
   title = 'Angular-pwa';
 
   apiData: any;
-  constructor(private http: HttpClient) { }
+  private  publicKey: 'BDw9rsFllBr0w5Yu7f6lgC8O_yAF4QLOmrHDin8MR3cikeJOQcZNpzSlEM4RUUajWz2n8L52wCyLSFWemGuyTJI';
+  constructor(private http: HttpClient, private update: SwUpdate
+    , private swPush: SwPush) { 
+    }
 
   ngOnInit() {
+    this.PushSubscription();
     this.http.get('http://dummy.restapiexample.com/api/v1/employees').subscribe(
       (res: any) => {
         this.apiData = res.data;
@@ -19,5 +24,39 @@ export class AppComponent {
         console.error(err);
       }
     )
+    
+    this.updateClient();
   }
+
+updateClient(){
+  if(!this.update.isEnabled){
+    console.log('not enabled');
+    return;
+  }
+this.update.available.subscribe((Event) => {
+  
+  console.log(`current`, Event.current, `available`, Event.available);
+  if(confirm('update avaliable for the app please confirm')){
+    this.update.activateUpdate().then(() => console.log('Activated'));
+  }
+});
+this.update.activated.subscribe((Event) => {
+  
+  console.log(`current`, Event.previous, `available`, Event.current);
+});
+  
+  
+}
+PushSubscription(){
+if(!this.swPush.isEnabled){
+  console.log('Notification is not enabled');
+  return;
+}
+this.swPush.requestSubscription({
+  serverPublicKey: this.publicKey,
+}).then(sub => {console.log(JSON.stringify(sub));}).catch(err => console.log(err));
+}
+
+
+
 }
